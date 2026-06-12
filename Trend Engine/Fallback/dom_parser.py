a30165f4c50b9e7c26658a2526728613a464ebae
Hutansh <hutanshsharma241005@gmail.com>
2026-06-13 00:33:46 +0530

    added the fallback reddit parser

--- Trend Engine/Fallback/dom_parser.py
+++ Trend Engine/Fallback/dom_parser.py
@@ -4,7 +4,7 @@ from datetime import datetime,UTC
 from scraper import scraper
 
 class DocumentEncoder:
-    def extract(self, soup):
+    def generic_extractor(self, soup):
         title = ""
         if soup.title:
             title = soup.title.get_text(" ",strip=True)
@@ -54,11 +54,46 @@ class DocumentEncoder:
             "metadata": metadata
         }
 
-    def save(self,data):
+    def reddit_extractor(self, soup):
+        data=soup.find("shreddit-feed")
+        output=[]
+        extraction_time = datetime.now(UTC).strftime("%Y-%m-%d_%H.%M.%S") 
+        if data:
+            posts=data.find_all("shreddit-post")
+            for post in posts:
+                title=post.get("post-title") if post.has_attr("post-title") else ""
+                id=post.get("id") if post.has_attr("id") else ""
+                upvotes=post.get("score") if post.has_attr("score") else 0
+                subreddit=post.get("subreddit-name") if post.has_attr("subreddit-name") else ""
+                comments=post.get("comment-count") if post.has_attr("comment-count") else 0
+                created_utc=post.get("created-timestamp") if post.has_attr("created-timestamp") else ""
+                formatted = datetime.strptime(created_utc,"%Y-%m-%dT%H:%M:%S.%f%z").strftime("%Y-%m-%d_%H.%M.%S") if created_utc else ""
+
+                if id and title:
+                    output.append({
+                    "id":id,
+                    "title":title,
+                    "source":"reddit",
+                    "region":"IN",
+                    "feed":"popular",
+                    "subreddit":subreddit,
+                    "created_utc":formatted,
+                    "extracted_utc":extraction_time,
+                    "upvotes":upvotes,
+                    "comments":comments
+                })
+        return output
+        
+
+    def youtube_extractor(self, soup):
+        pass
+
+
+    def save(self,data,name):
         BASE_DIR = Path(__file__).resolve().parent
         dir=(BASE_DIR.parent.parent/"data"/"raw")
         dir.mkdir(parents=True, exist_ok=True)
-        filename = datetime.now(UTC).strftime("%Y-%m-%d_%H-%M-%S-scraper.json")
+        filename = datetime.now(UTC).strftime(f"%Y-%m-%d_%H-%M-%S-scraper {name}.json")
 
         filepath=dir/filename
 
@@ -67,5 +102,5 @@ class DocumentEncoder:
 
 if __name__=="__main__":
     encoder = DocumentEncoder()
-    data=scraper("https://www.reddit.com/",encoder.extract)
-    encoder.save(data)
+    data=scraper("https://www.reddit.com/r/popular/",encoder.reddit_scrapper)
+    encoder.save(data,"reddit")
